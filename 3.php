599ad38267a2e0876b0a666742db0bfa3636cdfb
kammueller <matthias@kammueller.eu>
2017-03-26 11:33:05 +0200

    fixed: added tabel for reservierungen

--- 3.php
+++ 3.php
@@ -1,5 +1,5 @@
 <?php
-header ('Content-type: text/html; charset=utf-8');
+header('Content-type: text/html; charset=utf-8');
 include("abiball/back-end/db_connect.inc.php");
 
 if (!(isset($_POST["host"]) && isset($_POST["user"]) && isset($_POST["db"]) && isset($_POST["pass"]))) {
@@ -13,7 +13,7 @@ if (($_POST["host"] == "") OR ($_POST["user"] == "") OR ($_POST["db"] == "") OR 
     exit;
 }
 
-    $query = "CREATE TABLE IF NOT EXISTS `abi_0_kartenfreischalt` (
+$query = "CREATE TABLE IF NOT EXISTS `abi_0_kartenfreischalt` (
 			  `timestamp` INT(12) NOT NULL,
 			  `anzahl` INT(3) NOT NULL,
 			  `uebrig` INT(3) NOT NULL,
@@ -124,6 +124,19 @@ if (mysqli_query($db_link, $query)) {
     exit;
 }
 
+$query = "CREATE TABLE IF NOT EXISTS `abi_tische` (
+  `Nummer` int(2) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `Plaetze` int(2) NOT NULL,
+  `Frei` int(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8; ";
+if (mysqli_query($db_link, $query)) {
+    print "Datenbank f&uuml;r Tischreservierungen wurde erstellt <br>";
+} else {
+    echo "<h2>Fehler in der Verbindung. Bitte nochmals versuchen!</h2><br>
+		<a href='2.php'>Bitte probieren Sie es erneut.</a>";
+    exit;
+}
+
 $query = "CREATE TABLE IF NOT EXISTS `abi_reservierung` (
 			  `user_id` INT(8) NOT NULL,
 			  `ablauf` INT(20) NOT NULL COMMENT 'timestamp',
@@ -190,7 +203,6 @@ if (mysqli_query($db_link, $query)) {
     exit;
 }
 
-
 $trun = mysqli_query($db_link, "TRUNCATE TABLE `abi_0_kartenfreischalt`;");
 $trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_admin`;");
 $trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_bestellung`;");
@@ -199,6 +211,7 @@ $trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_karten`;");
 $trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_news`;");
 $trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_reservierung`;");
 $trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_session`;");
+$trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_tische`;");
 $trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_user`;");
 $trun = $trun && mysqli_query($db_link, "TRUNCATE TABLE `abi_verify`;");
 if ($trun) {
@@ -208,7 +221,6 @@ if ($trun) {
 		<a href='2.php'>Bitte probieren Sie es erneut.</a>";
     exit;
 }
-
 
 $query = "INSERT INTO `abi_admin` (`user_id`, `rechte`) VALUES
 			(1, 'all');
@@ -236,7 +248,8 @@ if (mysqli_query($db_link, $query)) {
  * @TODO Drop Keys!!!
  */
 $query = "INSERT INTO `abi_user` (`id`, `password`, `Vorname`, `Nachname`, `verified`, `Mail`, `reservierend`, `failed`) VALUES
-			(1, '" . password_hash($_POST["pass"], PASSWORD_BCRYPT) . "', '" . $_POST["host"] . "', '" . $_POST["user"] . "', 'true', '" . $_POST["db"] . "', NULL, 0);
+			(1, '" . password_hash($_POST["pass"], PASSWORD_BCRYPT) . "', '" . $_POST["host"] . "', '" .
+    $_POST["user"] . "', 'true', '" . $_POST["db"] . "', NULL, 0);
 			";
 if (mysqli_query($db_link, $query)) {
     print "Nutzer wurde angelegt <br><br><br>";
@@ -245,7 +258,6 @@ if (mysqli_query($db_link, $query)) {
 		<a href='2.php'>Bitte probieren Sie es erneut.</a>";
     exit;
 }
-
 
 ### HEADERDATA SCHREIBEN ###
 /**@todo escape */
@@ -257,34 +269,34 @@ $content = '<?php
  */
 
  // Die Folgenden Daten werden NICHT encoded!!
-define("html_title", "'.$_POST["html"].'"); // Der HTML-Title
+define("html_title", "' . $_POST["html"] . '"); // Der HTML-Title
 
-define("header", "'.$_POST["header1"].'"); // erste Zeile
+define("header", "' . $_POST["header1"] . '"); // erste Zeile
 
-define("sub_head", "'.$_POST["header2"].'"); // zweite Zeile
+define("sub_head", "' . $_POST["header2"] . '"); // zweite Zeile
 
  // ANSPRECHPARTNER:
 /** @var STRING $admin_vor
  * Vorname des Admins
  */
-$admin_vor  = "'.$_POST["host"].'";
+$admin_vor  = "' . $_POST["host"] . '";
 /** @var STRING $admin_nach
  * Nachname des Admins
  */
-$admin_nach = "'.$_POST["user"].'";
+$admin_nach = "' . $_POST["user"] . '";
 /** @var STRING $admin_mail
  * Mailadresse des Admins
  */
-define("admin_mail", "'.$_POST["db"].'");
+define("admin_mail", "' . $_POST["db"] . '");
 
 /** @var STRING $admin_post
  * Adresse des Admins (im Real-life) ;D
  */
-$admin_post = "'.$_POST["str"].' %nZ%
-                '.$_POST["ort"].'";
+$admin_post = "' . $_POST["str"] . ' %nZ%
+                ' . $_POST["ort"] . '";
 
 /* webadress - Die Überadresse ( http://[...].de) */
-define("webadress", "'.$_POST["adress"].'");               ';
+define("webadress", "' . $_POST["adress"] . '");               ';
 
 $handle = fopen($file, "w");
 if (!fwrite($handle, $content)) {
@@ -294,7 +306,6 @@ if (!fwrite($handle, $content)) {
 }
 fclose($handle);
 
-
 $file = "abiball/back-end/db_captcha.php";
 $content = '<?php
 /**
@@ -302,8 +313,8 @@ $content = '<?php
  */
 
 /* CAPTCHA-CODES */
-define("privatekey", "'.$_POST["cap2"].'");
-define("publickey", "'.$_POST["cap1"].'");
+define("privatekey", "' . $_POST["cap2"] . '");
+define("publickey", "' . $_POST["cap1"] . '");
 /* ADMIN-PW */
 define("check", \'$2y$10$QCTJVtTM40yN7effvSEx/OeLG62/cKzH4CdkSbrDVsvdpBLHgZMme\');';
 $handle = fopen($file, "w");
